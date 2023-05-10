b033972d722de602fa43727031a4e6ff08d16a5a
Valentin Machefaux <valentin.machefaux@gmail.com>
2023-05-10 09:37:17 +0200

    Database param

--- server/database/client.php
+++ server/database/client.php
@@ -6,14 +6,14 @@ class Database {
         
         // variables de connection a la bdd
         $host = "localhost";
-        $dbname = "db_login";
-        $username = "root";
-        $password = "";
-        $port = 3306;
+        $dbname = "socialNetwork";
+        $username = "postgres";
+        $password = "0000";
+        $port = 5432;
 
         $connection = null;
         try {
-            $connection = new PDO("mysql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $username, $password);
+            $connection = new PDO("pgsql:host=" . $host . ";port=" . $port . ";dbname=" . $dbname, $username, $password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch(PDOException $exception){
             echo "Erreur de connexion:" . $exception->getMessage();
