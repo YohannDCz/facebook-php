cb486b9f390ccc98cf17934c719c2b0e77070f27
SirMacCready <polocharbel@gmail.com>
2023-05-16 15:36:39 +0200

    Update Users.php

--- src/model/Users.php
+++ src/model/Users.php
@@ -24,16 +24,15 @@ class Users {
         }
 
         return $users;
-
+    }
         // Fermeture de la connection
 //        $connection = null;
 
         // Envoi des données au format JSON
         // header('Content-Type: application/json');
         // echo json_encode($users);
-    }
 
-    public function add_user($username, $password, $first_name, $last_name, $phone, $mail){
+    function add_user($username, $password, $first_name, $last_name, $phone, $mail){
         //Connecter la BDD
         $db = new Database();
         // Ouverture de la connection
@@ -125,5 +124,32 @@ class Users {
     
         return $query;
     }
-        
+
+    function deleteUser($password,$mail) {
+        //Connecter la BDD
+        $db = new Database();
+        //Ouverture de la connection
+        $connection = $db->getConnection();
+        //Requêtes SQL
+        $check = 'SELECT mail,password from user WHERE mail = :mail ;';
+        //Vérification de l'identifiant unique 
+        $query = $connection->prepare($check);
+        $query->bindParam(":mail", $mail);
+        if ($query->execute()){
+            //On voit si les mots de passe sont les mêmes
+            if (password_verify(query["$password"], password_hash($password))) {
+                //Requêtes SQL
+                $sql = 'DROP FROM user WHERE mail = :mail and password = :password ;';
+                $query = $connection->prepare($sql);
+                $mail1=htmlspecialchars(strip_tags($mail));
+                $query->bindParam(":password", $password);
+                $query->bindParam(":mail", $mail1);
+                if ($query->execute()){
+                    return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
 }
