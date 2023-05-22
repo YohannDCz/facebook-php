c841eb07d955e1ba640595f117f5edcada6c620f
Chrisline Lin <81916964+LinelinLove@users.noreply.github.com>
2023-05-22 21:30:19 +0200

    minor fix and add publication script

--- front/scripts/script.js
+++ front/scripts/script.js
@@ -1,38 +1,46 @@
 // adapter le textarea en fonction du contenu
 function autoResize(textarea) {
-    textarea.style.height = 'auto'; // Réinitialisez d'abord la hauteur à 'auto' pour éviter le débordement initial
-    textarea.style.height = textarea.scrollHeight + 'px'; // Ajuste la hauteur en fonction de la hauteur du contenu
+  textarea.style.height = 'auto'; // Réinitialisez d'abord la hauteur à 'auto' pour éviter le débordement initial
+  textarea.style.height = textarea.scrollHeight + 'px'; // Ajuste la hauteur en fonction de la hauteur du contenu
 }
 
-const themeToggle = document.getElementById('theme-toggle');
-const bodyElement = document.body;
+if (!window.themeToggle) {
+  let themeToggle = document.getElementById('theme-toggle');
+  let bodyElement = document.body;
 
-// Fonction pour appliquer le thème
-function applyTheme(theme) {
-  if (theme === 'dark') {
-    bodyElement.classList.add('dark-theme');
-    bodyElement.classList.remove('light-theme');
-    themeToggle.checked = true;
+  // Fonction pour appliquer le thème
+  function applyTheme(theme) {
+    if (theme === 'dark') {
+      bodyElement.classList.add('dark-theme');
+      bodyElement.classList.remove('light-theme');
+      if (themeToggle) {
+        themeToggle.checked = true;
+      }
+    } else {
+      bodyElement.classList.add('light-theme');
+      bodyElement.classList.remove('dark-theme');
+      if (themeToggle) {
+        themeToggle.checked = false;
+      }
+    }
+  }
+
+  // Vérifier si le thème est déjà stocké
+  let storedTheme = localStorage.getItem('theme');
+  if (storedTheme) {
+    applyTheme(storedTheme);
   } else {
-    bodyElement.classList.add('light-theme');
-    bodyElement.classList.remove('dark-theme');
-    themeToggle.checked = false;
+    // Aucun thème stocké, appliquer le thème en fonction du préférences du système
+    let systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    applyTheme(systemTheme);
+  }
+
+  // Gestionnaire d'événements pour le bouton de basculement du thème (si l'élément existe)
+  if (themeToggle) {
+    themeToggle.addEventListener('change', function () {
+      let selectedTheme = themeToggle.checked ? 'dark' : 'light';
+      applyTheme(selectedTheme);
+      localStorage.setItem('theme', selectedTheme);
+    });
   }
 }
-
-// Vérifier si le thème est déjà stocké
-const storedTheme = localStorage.getItem('theme');
-if (storedTheme) {
-  applyTheme(storedTheme);
-} else {
-  // Aucun thème stocké, appliquer le thème en fonction du préférences du système
-  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  applyTheme(systemTheme);
-}
-
-// Gestionnaire d'événements pour le bouton de basculement du thème
-themeToggle.addEventListener('change', function() {
-  const selectedTheme = themeToggle.checked ? 'dark' : 'light';
-  applyTheme(selectedTheme);
-  localStorage.setItem('theme', selectedTheme);
-});
