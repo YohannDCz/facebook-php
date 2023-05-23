a0d4af83e61d97a8457d2fe075e921bf0f064c5d
Valentin Machefaux <valentin.machefaux@gmail.com>
2023-05-23 17:55:57 +0200

    Changes by HUGO

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
+ini_set('display_errors', 1);
+// inclure les controllers nécessaires
+
+// récupérer la méthode et l'URL de la requête
+$method = $_SERVER['REQUEST_METHOD'];
+$url = $_SERVER['REQUEST_URI'];
+
+// sépare l'url en plusieurs sections pour le routage
+$split_url = explode($url, '/');
 
 
+//Router
+switch($split_url[1]){
+    // Route utilisateur de l'API
+    case 'user':
+        // require le controller user
+        require_once './src/controller/userController.php';
+        break;
+    case 'page':
+        //  require le controller page
+        require_once './src/controller/pagesController.php';
+        break;
+    case 'group':
+        //  require le controller group
+        require_once './src/controller/groupController.php';
+        break;
+    case 'post':
+        //  require le controller post
+        require_once './src/controller/postController.php';
+        break;
+}
 
-    ?>
-</body>
-</html>
+?>
--- src/websocket/directChat.php
+++ src/websocket/directChat.php
@@ -4,7 +4,20 @@ use Database;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+//require_once "../model/Database.php";
+
+
 class Chat implements MessageComponentInterface {
+
+    public function connectDB() {
+
+        //  Connecter la BDD
+        $db = new Database();
+        // Ouverture de la connection
+        return $db->getConnection();
+
+    }
+
     public function onOpen(ConnectionInterface $conn) {
 
         $connection = $this->connectDB();
@@ -18,9 +31,31 @@ class Chat implements MessageComponentInterface {
 
 
     public function onMessage(ConnectionInterface $from, $msg) {
+
+        $connection = $this->connectDB();
+
+        switch($_COOKIE["group"]){
+            case true :
+                // Request pour group message
+                // récupérer le group_id les user_id et le contenu du message
+                
+
+            default :
+                // Request pour message privé
+        }
+
     }
 
     public function onClose(ConnectionInterface $conn) {
+
+        $connection = $this->connectDB();
+
+        $request = $connection->prepare("UPDATE user SET is_online = false WHERE user.id = :id");
+
+        $request->bindParam(":id", $_COOKIE["id"]);
+
+        $request->execute();
+
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
