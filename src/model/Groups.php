25f4d79a4e42402cbeb45e9ee3e1b0f59a9c16b6
Valentin Machefaux <valentin.machefaux@gmail.com>
2023-05-23 17:55:39 +0200

    Quelques models et controllers

--- src/model/Groups.php
+++ src/model/Groups.php
@@ -1,4 +1,8 @@
 <?php
+
+//Inclusion du fichier pour la connexion a la BDD
+require_once 'Database.php';
+
 class Groups
 {
     // fonction qui récupère tous les groupes d'un utilisateur
@@ -11,7 +15,7 @@ class Groups
         $connection = $db->getConnection();
 
         // Requêtes SQL
-        $request = $connection->prepare("SELECT * FROM groups WHERE groups.id IN ( SELECT user_group.group_id FROM user_group WHERE user_group.user_id = :id )");
+        $request = $connection->prepare("SELECT * FROM \"groups\" WHERE \"groups\".id IN ( SELECT user_group.group_id FROM user_group WHERE user_group.user_id = :id )");
         $request->bindParam(":id", $id);
         $request->execute();
 
@@ -40,5 +44,42 @@ class Groups
         //  fermeture de la connexion
         $connection = null;
         return $users;
+    }
+
+//    fonction qui creer un groupe
+    function createGroup($name, $description, $private, $timestamp){
+        // connect bdd
+        $db = new Database();
+
+        // Ouverture de la connection
+        $connection = $db->getConnection();
+
+        $request = $connection->prepare("INSERT INTO \"groups\" (name, description, private,timestampz) VALUES (:name, :description, :private, :timestamp)");
+        $request->bindParam(":name", $name);
+        $request->bindParam(":description", $description);
+        $request->bindParam(":private", $private);
+        $request->bindParam(":timestamp", $timestamp);
+
+        if ($request->execute()){
+            return false;
+        }
+    }
+
+    // fonction pour tej un groupe
+    function deleteGroup()
+    {
+        // connect bdd
+        $db = new Database();
+
+        // Ouverture de la connection
+        $connection = $db->getConnection();
+
+        $request = $connection->prepare("DELETE FROM \"groups\" WHERE id = :group_id");
+
+
+        if ($request->execute()){
+            return false;
+        }
+
     }
 }
--- src/websocket/directChat.php
+++ src/websocket/directChat.php
@@ -1,11 +1,21 @@
 <?php
 namespace MyApp;
+use Database;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
     public function onOpen(ConnectionInterface $conn) {
+
+        $connection = $this->connectDB();
+
+        $request = $connection->prepare("UPDATE user SET is_online = true WHERE user.id = :id");
+
+        $request->bindParam(":id", $_COOKIE["id"]);
+
+        $request->execute();
     }
+
 
     public function onMessage(ConnectionInterface $from, $msg) {
     }
