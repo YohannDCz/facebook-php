ab943f464341e2cc1c62eff9c86929dd90b8de61
Hugo <cieplucha.hugo@gmail.com>
2023-05-24 11:22:36 +0200

    refactoring

--- src/controller/userController.php
+++ src/controller/userController.php
@@ -2,10 +2,6 @@
 require_once('../model/Database.php');
 require_once('../model/Users.php');
 
-// déclaration variable globale
-global $split_url;
-
-
 
 function signup() {
 
@@ -89,32 +85,5 @@ if(isset($_SERVER['HTTP_REFERER']) and $_SERVER["REQUEST_METHOD"] === "POST") {
       exit();
   }
 }
-
-
-
-// router dans la catégorie user
-switch ($split_url[2]) {
-    case 'profile':
-      //require la page profil
-      require_once '';
-      break;
-    case 'signup':
-      //require la page signup
-      require_once '';
-      break;
-    case 'login':
-      //require la page login
-      require_once '';
-      break;
-    case 'mentions':
-      //require la page mentions légale
-      require_once '';
-      break;
-    default :
-      echo 'DEFAULT USER PAGE';
-      break;
-}
-
-
 
 ?>
---
+++ src/router/userRoutes.php
@@ -0,0 +1,27 @@
+<?php
+
+// déclaration variable globale
+global $split_url;
+
+// router dans la catégorie user
+switch ($split_url[2]) {
+    case 'profile':
+      //require la page profil
+      require_once '';
+      break;
+    case 'signup':
+      //require la page signup
+      require_once '';
+      break;
+    case 'login':
+      //require la page login
+      require_once '';
+      break;
+    case 'mentions':
+      //require la page mentions légale
+      require_once '';
+      break;
+    default :
+      echo 'DEFAULT USER PAGE';
+      break;
+}
