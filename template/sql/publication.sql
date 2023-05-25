3c302a6273b12047a2e4cfb012fc4be12ce84028
Yohann DC <YohannDCz@gmail.com>
2023-05-25 11:07:56 +0200

    Create publication.sql

---
+++ template/sql/publication.sql
@@ -0,0 +1,42 @@
+INSERT INTO publications (content)
+VALUES
+    ('{
+        "description": "Un superbe coucher de soleil sur la plage.",
+        "image": "https://media.istockphoto.com/id/1172427455/fr/photo/beau-coucher-du-soleil-au-dessus-de-la-mer-tropicale.jpg?s=170667a&w=0&k=20&c=9rQSiwMoI_U9ENSlDELPyxRSBNM4-pgKmcAzrLdlG7k="
+    }'),
+    ('{
+        "description": "Découvrez les meilleurs restaurants de la ville avec notre guide culinaire.",
+        "image": ""
+    }'),
+    ('{
+        "description": "Une magnifique vue panoramique depuis la montagne.",
+        "image": "https://images.immediate.co.uk/production/volatile/sites/4/2021/08/mountains-7ddde89.jpg?quality=90&resize=768,574"
+    }'),
+    ('{
+        "description": "Plongez dans l''aventure avec notre nouvelle collection de jeux vidéo.",
+        "image": "https://media.cnn.com/api/v1/images/stellar/prod/i-stock-1287493837-1.jpg?q=h_1194,w_2121,x_0,y_0"
+    }'),
+    ('{
+        "description": "Le voyage de vos rêves vous attend. Réservez dès maintenant !",
+        "image": ""
+    }'),
+    ('{
+        "description": "Des conseils pratiques pour entretenir votre jardin et avoir de belles plantes.",
+        "image": ""
+    }'),
+    ('{
+        "description": "Découvrez les dernières tendances de la mode pour cette saison.",
+        "image": "https://www.francetvinfo.fr/pictures/RVl3uH3vWXxIxAsZUa173e_r4Bk/752x423/2022/09/28/phpjABfai.jpg"
+    }'),
+    ('{
+        "description": "Apprenez à cuisiner comme un chef avec nos recettes exclusives.",
+        "image": ""
+    }'),
+    ('{
+        "description": "Un guide complet pour vous aider à choisir le meilleur smartphone.",
+        "image": "https://image.coolblue.nl/content/84b532be9da585989322a100921aced0"
+    }'),
+    ('{
+        "description": "Inspirez-vous avec nos idées de décoration pour créer un intérieur unique.",
+        "image": ""
+    }');
