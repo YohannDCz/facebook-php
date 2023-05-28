91b6161ee15aedb3cf1dc14719a12314df870546
Hugo Cieplucha <cieplucha.hugo@gmail.com>
2023-05-28 22:51:56 +0200

    fix

--- src/model/Pages.php
+++ src/model/Pages.php
@@ -263,7 +263,7 @@ class Pages
         $requestData = $request->fetch(PDO::FETCH_ASSOC);
         $user_id = $requestData['id'];
 
-        $request = $connection->prepare("SELECT * FROM users_page WHERE user_id = :user_id AND page_id = :page_id");
+        $request = $connection->prepare("SELECT role FROM users_page WHERE user_id = :user_id AND page_id = :page_id");
 
         $request->bindParam(":user_id", $user_id); 
         $request->bindParam(":page_id", $page_id); 
