278d507ccf8461e56afad6724fba01843e6b875b
Linelinlove <chrisline.lin@outlook.fr>
2023-05-28 12:23:06 +0200

    add button delete account

--- template/404.php
+++ template/404.php
@@ -1,2 +1,14 @@
-<?php 
-echo "error 404";
+<?php include 'template/components/header.php' ?>
+
+<div class="container_404">
+    <h1>Erreur 404</h1>
+    <h3>La page que vous recherchez semble introuvable.</h3>
+    
+    <p>Voici quelques liens utiles à la place :</p>
+    <ul>
+        <li><a href=<?= "http://" . $host . "/home" ?>>Page d'accueil</a></li>
+        <!-- <li><a href=<?= "http://" . $host . "/home" ?>>Homepage</a></li> -->
+    </ul>
+</div>
+
+<?php include 'template/components/footer.php' ?>
